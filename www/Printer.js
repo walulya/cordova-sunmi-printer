8602d4aa9ab29b840ed74582eb3651df35abfadc
Ivan <iwalulya@yahoo.co.uk>
2018-12-29 21:19:54 +0300

    change structure

--- www/Printer.js
+++ www/Printer.js
@@ -1,5 +1,17 @@
 var exec = require('cordova/exec');
 
+var PLUGIN_NAME = 'Printer';
+
+var RTPrinter = {
+    add: function (args, cb) {
+        //exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
+        exec(cb, null, "Printer", "performAdd", args);
+    },
+}
+
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'Printer', 'coolMethod', [arg0]);
 };
+
+
+module.exports = RTPrinter;
