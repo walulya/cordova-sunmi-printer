fca38130e5814e667d34f6cf4f06a042eaa4dc94
Ivan <iwalulya@yahoo.co.uk>
2018-12-29 20:37:00 +0300

    here

---
+++ src/android/Printer.java
@@ -0,0 +1,41 @@
+package pebuu.printer;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class Printer extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        if (action.equals("performAdd")) {
+            int arg1 = args.getInt(0);
+            int arg2 = args.getInt(1);
+            /* Indicating success is failure is done by calling the appropriate method on the 
+            callbackContext.*/
+            int result = arg1 + arg2;
+            callbackContext.success("result calculated in Java: " + result);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
