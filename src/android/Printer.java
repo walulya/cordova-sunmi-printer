a0fa6a38a8d3dd927bd68c70cfd8873eb3f1525f
Ivan <iwalulya@yahoo.co.uk>
2018-12-30 09:35:37 +0300

    change structure

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -3,6 +3,13 @@ package pebuu.printer;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Handler;
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,6 +24,11 @@ public class Printer extends CordovaPlugin {
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
+            listBT(callbackContext);
+            return true;
+        }
+        if (action.equals("list")) {
+            listBT(callbackContext);
             return true;
         }
         if (action.equals("performAdd")) {
@@ -38,4 +50,47 @@ public class Printer extends CordovaPlugin {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+//This will return the array list of paired bluetooth printers
+	void listBT(CallbackContext callbackContext) {
+		BluetoothAdapter mBluetoothAdapter = null;
+		String errMsg = null;
+		try {
+			mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+			if (mBluetoothAdapter == null) {
+				errMsg = "No bluetooth adapter available";
+				//Log.e(LOG_TAG, errMsg);
+				callbackContext.error(errMsg);
+				return;
+			}
+			if (!mBluetoothAdapter.isEnabled()) {
+				Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				this.cordova.getActivity().startActivityForResult(enableBluetooth, 0);
+			}
+			Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+			if (pairedDevices.size() > 0) {
+				JSONArray json = new JSONArray();
+				for (BluetoothDevice device : pairedDevices) {
+					
+					Hashtable map = new Hashtable();
+					map.put("type", device.getType());
+					map.put("address", device.getAddress());
+					map.put("name", device.getName());
+					JSONObject jObj = new JSONObject(map);
+					json.put(jObj);
+					//json.put(device.getName());
+				}
+				callbackContext.success(json);
+			} else {
+				callbackContext.error("No Bluetooth Device Found");
+			}
+			//Log.d(LOG_TAG, "Bluetooth Device Found: " + mmDevice.getName());
+		} catch (Exception e) {
+			errMsg = e.getMessage();
+			Log.e(LOG_TAG, errMsg);
+			e.printStackTrace();
+			callbackContext.error(errMsg);
+		}
+	}
+
 }
--- www/Printer.js
+++ www/Printer.js
@@ -5,8 +5,11 @@ var PLUGIN_NAME = 'Printer';
 var RTPrinter = {
     add: function (args, cb) {
         //exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
-        exec(cb, null, "Printer", "performAdd", args);
+        exec(cb, null, PLUGIN_NAME, "performAdd", args);
     },
+    list: function(fnSuccess, fnError){
+        exec(fnSuccess, fnError, PLUGIN_NAME, "list", []);
+     },
 }
 
 exports.coolMethod = function (arg0, success, error) {
