e10f01e9a3c66abeaca46f68ce04dee9e41cea6b
Ivan <iwalulya@yahoo.co.uk>
2019-01-02 14:58:05 +0300

    adding image printing

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -14,6 +14,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.content.res.Resources;
 
 import org.json.JSONArray;
 import org.json.JSONException;
