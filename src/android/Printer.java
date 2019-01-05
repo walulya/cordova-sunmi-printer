f7d52fc3c1638a1391b045d9623d18384374e887
Ivan <iwalulya@yahoo.co.uk>
2019-01-06 00:05:23 +0300

    done testing

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -469,11 +469,11 @@ public class Printer extends CordovaPlugin implements PrinterObserver{
         try {
             rtPrinter.connect(bluetoothEdrConfigBean);
             callbackContext.success("Printer connected successfully" + rtPrinter.getConnectState());
-            //showToast(printerInterface.getConfigObject().toString() +" "+ rtPrinter.getConnectState());
+            showToast(printerInterface.getConfigObject().toString() +" "+ rtPrinter.getConnectState());
         } catch (Exception e) {
             String errMsg = e.getMessage();
 			Log.e(LOG_TAG, errMsg);
-			e.printStackTrace();
+			//e.printStackTrace();
 			callbackContext.error(errMsg);
         } finally {
 
