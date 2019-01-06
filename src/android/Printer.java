7ebc4625e0989ad02954d162adcbb9a8fef10545
Ivan <iwalulya@yahoo.co.uk>
2019-01-06 10:13:47 +0300

    done testing

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -489,7 +489,7 @@ public class Printer extends CordovaPlugin implements PrinterObserver{
 
     @Override
     public void printerObserverCallback(final PrinterInterface printerInterface, final int state) {
-        /* cordova.getActivity().runOnUiThread(new Runnable() {
+         cordova.getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
                  switch (state) {
@@ -520,7 +520,7 @@ public class Printer extends CordovaPlugin implements PrinterObserver{
                         break;
                 }               
             }
-         });*/
+         });
     
     }
 
